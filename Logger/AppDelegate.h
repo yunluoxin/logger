f181570d42ec113b7a9dd69588b261520285fbd3
hhdgw@qq.com <hhdgw@qq.com>
2016-10-06 14:02:00 +0800

    Initial Commit

---
+++ Logger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Logger
+//
+//  Created by dadong on 16/10/6.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Logger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Logger
+//
+//  Created by dadong on 16/10/6.
+//  Copyright © 2016年 dadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
